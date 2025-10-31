30f9a6333bc758a64418f6c571df20b7e4f8c5a5
Lachance, Alexandre <alexandrelachance@me.com>
2025-10-31 17:11:10 -0400

    Improve run script

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cargo build --release
-
-qemu-system-riscv64 -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel target/riscv64imac-unknown-none-elf/release/meos
+if cargo build --release; then
+    qemu-system-riscv64 -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel target/riscv64imac-unknown-none-elf/release/meos
+fi

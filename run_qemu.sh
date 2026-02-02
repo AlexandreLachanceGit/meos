689eb2b211bf3173d00e54ca2f56af3b2d47b768
Lachance, Alexandre <alexandrelachance@me.com>
2026-02-01 22:16:13 -0500

    Move from run script to cargo run directly

--- run_qemu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if LOG_LEVEL=3 cargo build --release; then
-    qemu-system-riscv64 -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel target/riscv64imac-unknown-none-elf/release/meos
-fi
